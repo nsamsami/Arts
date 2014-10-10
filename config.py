86646f74cb79ebc87a013ead60e7b346e073eee4
Nahid Samsami <nahid.samsami@gmail.com>
2014-10-10 15:12:00 -0700

    changed files for postgres/heroku

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import os
 UPLOADS_DEFAULT_DEST = "./static/"
 DB_URI = os.environ.get("DATABASE_URL", "sqlite:///my_app.db")
 SECRET_KEY = "this should be a secret"
+
+# Whoosh does not work on Heroku
+# WHOOSH_ENABLED = os.environ.get('HEROKU') is None
