ad9c68d04a726b595fdcd24b5031ce8937160a04
Nahid Samsami <nahid.samsami@gmail.com>
2014-10-15 00:47:03 -0700

    postgres running locally

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
 UPLOADS_DEFAULT_DEST = "./static/"
-DB_URI = os.environ.get("DATABASE_URL", "sqlite:///my_app.db")
+DB_URI = os.environ.get("DATABASE_URL", "postgresql://localhost:5432/nahidsamsami")
 SECRET_KEY = "this should be a secret"
-
 # Whoosh does not work on Heroku
 # WHOOSH_ENABLED = os.environ.get('HEROKU') is None
